7d81e2243b0442bed985aa37595ad49258235aab
justbo <b.tsybulskyy@gmail.com>
2016-03-04 22:04:05 +0000

    add my Slider

--- js/main.js
+++ js/main.js
@@ -16,4 +16,29 @@ $(function(){
 	  $('html, body').animate({ scrollTop:$(scroll_element).offset().top }, 900); 
 	  return false;
   });
+  $('.controller').click(function() {
+  	var item  = $('.slider .item');
+  	var nmbSl = item.length;
+    var i = -1;
+    var curAct = 0;
+    //curent active item
+    $('.slider .item').each(function() {
+      ++i;
+      if($(this).hasClass('active')){
+      	  curAct = i;
+				};
+      });
+    //next active item
+    var nextAct = curAct;
+    if($(this).hasClass('left')){
+    	var nextAct = (curAct-1) % nmbSl;
+      if(nextAct<0){
+      	nextAct = nmbSl-1;
+      }
+    }else if ($(this).hasClass('right')){
+    	var nextAct = (curAct+1) % nmbSl;
+    };
+    $(item[curAct]).removeClass('active').animate({opacity:0});
+    $(item[nextAct]).animate({opacity:1},1000).addClass('active');
+  });
 });
