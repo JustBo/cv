900c68407662bba9864b4d9f8cc08e7bda3a5817
justbo <b.tsybulskyy@gmail.com>
2016-03-05 11:35:53 +0000

    photo

--- js/main.js
+++ js/main.js
@@ -16,32 +16,38 @@ $(function(){
 	  $('html, body').animate({ scrollTop:$(scroll_element).offset().top }, 900); 
 	  return false;
   });
+  var item  = $('.slider .item');
+  var nmbSl = item.length;
+  var curAct = curPosition();
+  var circle = $('.contr-circle li');
+  var nextAct = curAct;
   $('.controller').click(function() {
-  	var item  = $('.slider .item');
-  	var nmbSl = item.length;
-    var i = -1;
-    var curAct = 0;
-    var circle = $('.contr-circle li');
     //curent active item
-    $('.slider .item').each(function() {
-      ++i;
-      if($(this).hasClass('active')){
-      	  curAct = i;
-				};
-      });
+    curAct = curPosition();
     //next active item
-    var nextAct = curAct;
+    nextAct = curAct;
     if($(this).hasClass('left')){
-    	var nextAct = (curAct-1) % nmbSl;
+    	nextAct = (curAct-1) % nmbSl;
       if(nextAct<0){
       	nextAct = nmbSl-1;
       }
     }else if ($(this).hasClass('right')){
-    	var nextAct = (curAct+1) % nmbSl;
+    	nextAct = (curAct+1) % nmbSl;
     };
     $(item[curAct]).removeClass('active').animate({opacity:0});
     $(item[nextAct]).animate({opacity:1},1000).addClass('active');
     $(circle[curAct]).removeClass('circ-act');
     $(circle[nextAct]).addClass('circ-act');
   });
+  function curPosition() {
+    var res;
+    var i = -1;
+    $('.slider .item').each(function() {
+       ++i;
+       if($(this).hasClass('active')){
+        	  return res = i;
+  				}
+        });
+    return res;   
+  };
 });
